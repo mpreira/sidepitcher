19c83499d433e1a6db596d0b2f6268491049723c
Marie-Louise Preira <65363552+mpreira@users.noreply.github.com>
2026-03-08 14:10:15 +0100

    ajust

--- app/components/TimerControls.tsx
+++ app/components/TimerControls.tsx
@@ -99,7 +99,7 @@ export default function TimerControls({
             className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
             onClick={onApplyManualTime}
           >
-            <FontAwesomeIcon icon={faCheck} className="sm:mr-2" />
+            <FontAwesomeIcon icon={faCheck} className="mr-2 sm:mr-0" />
             <span className="inline sm:hidden">Appliquer</span>
           </button>
         </div>
